36d46128a8c29b3eeded3c15a647362948791132
Matt Morgan <m@platypi.io>
2023-01-12 17:52:51 -0800

    setting up gcloud deployment

---
+++ app.yaml.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo """
+runtime: nodejs
+env: flex
+service: $GCLOUD_APP_SERVICE
+resources:
+  cpu: 2
+  memory_gb: 2.3
+  disk_size_gb: 10
+automatic_scaling:
+  min_num_instances: 1
+  max_num_instances: 3
+  cool_down_period_sec: 180
+  cpu_utilization:
+    target_utilization: 0.6
+  target_concurrent_requests: 100
+"""
