a86094fb2cbf82fbffcec126b5f0753f1a97f1dc
Matt Morgan <m@platypi.io>
2023-01-19 18:56:06 -0800

    adding environment route

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -6,4 +6,8 @@ router.get('/api/hello', (req, res, next) => {
     res.json('World');
 });
 
+router.get('/api/environment', (req, res, next) => {
+    res.json(process.env.NODE_ENV);
+});
+
 export default router;
