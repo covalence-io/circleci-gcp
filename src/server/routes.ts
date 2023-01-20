dfbc5f1183c9ffb7b0b190344cbb52299593be49
Matt Morgan <m@platypi.io>
2023-01-19 19:24:52 -0800

    adding production pipeline

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -6,4 +6,8 @@ router.get('/api/hello', (req, res, next) => {
     res.json('World');
 });
 
+router.get('/api/env', (req, res, next) => {
+    res.json(process.env.NODE_ENV || 'error');
+});
+
 export default router;
